e8abc41272b20d5d1ab15c6e745bb7f6fbcc0c9a
Walter Zheng <zdwalter@gmail.com>
2012-03-10 15:16:24 +0800

    add

---
+++ public/javascripts/hard/ex5.js
@@ -0,0 +1,19 @@
+//http://learnpythonthehardway.org/book/ex5.html
+//Exercise 5: More Variables And Printing
+my_name = 'Zed A. Shaw'
+my_age = 35 // not a lie
+my_height = 74 // inches
+my_weight = 180 // lbs
+my_eyes = 'Blue'
+my_teeth = 'White'
+my_hair = 'Brown'
+
+print("Let's talk about "+my_name+".")
+print("He's "+my_height+" inches tall.")
+print("He's "+my_weight+" pounds heavy.")
+print("Actually that's not too heavy.")
+print("He's got "+my_eyes+" eyes and "+my_hair+" hair.")
+print("His teeth are usually "+my_teeth+" depending on the coffee.")
+
+// this line is tricky, try to get it exactly right
+print("If I add "+my_age+", "+my_height+", and "+my_weight+", I get "+(my_age + my_height + my_weight))
