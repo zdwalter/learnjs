49bfde9a7a355b99d5d889491318badbd963af6f
Walter Zheng <zdwalter@gmail.com>
2012-03-10 15:34:58 +0800

    rename

---
+++ public/javascripts/hard/ex09.js
@@ -0,0 +1,12 @@
+//http://learnpythonthehardway.org/book/ex9.html
+//Exercise 9: Printing, Printing, Printing
+
+//Here's some new strange stuff, remember type it exactly.
+
+days = "Mon Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+print("There's something going on here.\nWith the three double-quotes.\nWe'll be able to type as much as we like.\nEven 4 lines if we want, or 5, or 6.")
+
