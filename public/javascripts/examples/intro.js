ad906ace727210f5fa4e1ace9810cd43d531fe2b
Walter Zheng <zdwalter@gmail.com>
2012-03-16 10:52:32 +0800

    shorten

--- public/javascripts/examples/intro.js
+++ public/javascripts/examples/intro.js
@@ -1,9 +1,6 @@
-// Walter's 10-minute intro to Javascript
-
 // numbers!
 age = 26
 pi = 3.14159
-
 // strings!
 s = 'Rutherford Birchard Hayes'
 tokens = s.split(' ')
@@ -11,34 +8,26 @@ firstName = tokens[0]
 middleName = tokens[1]
 lastName = tokens[2]
 s2 = firstName + ' ' + middleName + ' ' + lastName
-
 // 'if' statement - indentation matters!
 if (s === s2)
     print('yes!!!')
 else
     print('nooooooo')
-
 // list (mutable sequence)
 beatles = ['John', 'Paul', 'George']
 beatles.push('Ringo')
-
 // 'for' loop - indentation matters!
 for (b in beatles)
     print('Hello', beatles[b])
-
 // dict - mapping unique keys to values
 netWorth = {}
-netWorth['Donald Trump'] = 3000000000
 netWorth['Bill Gates'] = 58000000000
 netWorth['Tom Cruise'] = 40000000
 netWorth['Joe Postdoc'] = 20000
-
-// iterating over key-value pairs:
 for (person in netWorth)
     worth = netWorth[person]
     if (worth < 1000000)
         print('haha', person, 'is not a millionaire')
-
 // testing dict membership
 if ('Tom Cruise' in netWorth)
     print('show me the money!')
